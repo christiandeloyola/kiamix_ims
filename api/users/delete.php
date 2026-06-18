097abee6e3a2b56fbd5f065a71b12ce323800c41
sijeyyy <sijeyyy20@gmail.com>
2026-06-19 00:27:09 +0800

    update

--- api/users/delete.php
+++ api/users/delete.php
@@ -1,6 +1,32 @@
 <?php
 
+session_start();
+
 header('Content-Type: application/json');
+
+if (!isset($_SESSION['user_id'])) {
+
+    http_response_code(401);
+
+    echo json_encode([
+        'success' => false,
+        'message' => 'Unauthorized'
+    ]);
+
+    exit;
+}
+
+if ($_SESSION['role'] !== 'Administrator') {
+
+    http_response_code(403);
+
+    echo json_encode([
+        'success' => false,
+        'message' => 'Administrator access required'
+    ]);
+
+    exit;
+}
 
 require_once '../../config/database.php';
 
@@ -8,6 +34,16 @@ $database = new Database();
 $pdo = $database->connect();
 
 $data = json_decode(file_get_contents("php://input"), true);
+
+if ($_SESSION['user_id'] == $data['id']) {
+
+    echo json_encode([
+        'success' => false,
+        'message' => 'You cannot delete your own account'
+    ]);
+
+    exit;
+}
 
 try {
 
