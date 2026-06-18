fc223641d1f8109e61de4f40cd4270486681dc25
sijeyyy <sijeyyy20@gmail.com>
2026-06-19 00:17:49 +0800

    update

--- api/auth/current_user.php
+++ api/auth/current_user.php
@@ -6,8 +6,11 @@ header('Content-Type: application/json');
 
 if(!isset($_SESSION['user_id'])){
 
+    http_response_code(401);
+
     echo json_encode([
-        'success' => false
+        'success' => false,
+        'message' => 'Not authenticated'
     ]);
 
     exit;
